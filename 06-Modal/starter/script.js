c7ad5c71eeedff0b2fb6e14380434f5012a81d61
Joshua Fielies <jfielies4@gmail.com>
2022-03-07 13:18:27 +0100

    finished modal windows

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,50 @@
-'use strict';
+"use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+
+const btnsOpenModal = document.querySelectorAll(".show-modal"); // to select all with this class
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+btnCloseModal.addEventListener("click", closeModal); // dont call the function () because then it wont be ativated on click
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  console.log(e.key);
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
+
+// before refactoring //
+// btnCloseModal.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// overlay.addEventListener("click", function () {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// document.addEventListener("keydown", function (e) {
+//     console.log(e.key);
+//     if (e.key === "Escape") {
+//       if (!modal.classList.contains("hidden")) {
+//         closeModal();
+//       }
+//     }
+//   });
