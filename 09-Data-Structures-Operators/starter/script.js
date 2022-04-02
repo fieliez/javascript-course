b35a66f635ff1c2eaab948ac3604fe36d2f11deb
Joshua Fielies <jfielies4@gmail.com>
2022-04-02 19:38:09 +0200

    final strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -6,31 +6,50 @@
 
 //////////////////
 ///////////            WORKING WITH STRINGS           ////////////
-console.log("a+very+nice+string".split("+"));
-console.log("Jonas Schmedtmann".split(" "));
+// console.log("a+very+nice+string".split("+"));
+// console.log("Jonas Schmedtmann".split(" "));
 
-const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
-const newName = ["Mr", firstName, lastName.toUpperCase()].join(" ");
-console.log(newName);
+// const [firstName, lastName] = "Jonas Schmedtmann".split(" ");
+// const newName = ["Mr", firstName, lastName.toUpperCase()].join(" ");
+// console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.toLowerCase().split(" ");
-  const namesUpper = [];
+// const capitalizeName = function (name) {
+//   const names = name.toLowerCase().split(" ");
+//   const namesUpper = [];
 
-  for (const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
-  console.log(namesUpper.join(" "));
-};
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(" "));
+// };
 
-const passenger = "jessica ann smith davis";
-capitalizeName(passenger);
-// padding
-const message = "Got to gate 23!";
-console.log(message.padStart(25, "+").padEnd(35, "+")); // fills the given length with the given padding
-console.log("Jonas".padStart(23, "+"));
+// const passenger = "jessica ann smith davis";
+// capitalizeName(passenger);
+// // padding
+// const message = "Got to gate 23!";
+// console.log(message.padStart(25, "+").padEnd(35, "+")); // fills the given length with the given padding
+// console.log("Jonas".padStart(23, "+"));
 
+// const maskCreditCard = function (number) {
+//   const str = number + "";
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, "*");
+// };
+// console.log(maskCreditCard(456887987646553132));
+// console.log(maskCreditCard(123456));
+// console.log(maskCreditCard("456887987646553132"));
+
+// /////// repeat
+// const message2 = "Bad weather... All Depatures Delayed...";
+// console.log(message2.repeat(5));
+
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${"🛩".repeat(n)}`);
+// };
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
 //////// strings 2
 // const airline = "Lufthanse Germany Airlines";
 
@@ -126,7 +145,30 @@ console.log("Jonas".padStart(23, "+"));
 // //how javascript does string methods
 // console.log(new String("jonas"));
 // console.log(typeof new String("jonas"));
+///////////////////////////////////////
+// String Methods Practice
 
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
+
+// console.log(flights.split("+"));
+const getCode = (str) => str.slice(0, 3).toUpperCase();
+for (const flight of flights.split("+")) {
+  const [type, from, to, time] = flight.split(";");
+  const output = `${type.startsWith("_Delayed") ? "🔴" : ""} ${type.replaceAll(
+    "_",
+    ""
+  )} ${from.slice(0, 3).toUpperCase()} to ${getCode(to)} (${time.replace(
+    ":",
+    "h"
+  )})`.padStart(40, "+");
+  console.log(output);
+}
 /////////////////////////////////////////////////////////
 // // Data needed for first part of the section
 const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
@@ -813,3 +855,55 @@ const gameEvents = new Map([
 // }
 
 ////////////////
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in
+ underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM 
+(see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words,
+ like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you
+ have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the
+ solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+// document.body.append(document.createElement("textarea"));
+// document.body.append(document.createElement("button"));
+
+// document.querySelector("button").addEventListener("click", function () {
+//   const text = document.querySelector("textarea").value;
+//   const rows = text.split("\n");
+//   for (const [i, row] of rows.entries()) {
+//     const [first, second] = row.toLowerCase().trim().split("_");
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd(20)}${"✅".repeat(i + 1)}`);
+//   }
+// });
+/////////////////////
